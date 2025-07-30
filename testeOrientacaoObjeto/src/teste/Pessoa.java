eeccd5f6fcb5852717e1e52f05911ade960450f9
miguelzackk <mzsilvaw@gmail.com>
2025-07-30 10:20:35 -0300

    Inicío do segundo semestre - Teste de orientacao a objeto.

---
+++ testeOrientacaoObjeto/src/teste/Pessoa.java
@@ -0,0 +1,17 @@
+package teste;
+
+public class Pessoa {
+
+
+	public String nome;
+	public int idade;
+	public int peso;
+	public String genero;
+	
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", idade=" + idade + ", peso=" + peso + ", genero=" + genero + "]";
+	}
+	
+	}
+
